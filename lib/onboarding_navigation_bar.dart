7a4f145277ea94e8eb549a1d6230ca7eea025a75
Mohammed Raees <raeesnazeer@gmail.com>
2023-02-22 19:38:47 +0530

    Update onboarding_navigation_bar.dart

--- lib/onboarding_navigation_bar.dart
+++ lib/onboarding_navigation_bar.dart
@@ -40,7 +40,7 @@ class OnBoardingNavigationBar extends StatelessWidget
     return AppBar(
       automaticallyImplyLeading: false,
       leading: leading,
-      action: currentPage == totalPage - 1
+      actions: currentPage == totalPage - 1
           ? finishButton == null
               ? SizedBox.shrink()
               : Container(
